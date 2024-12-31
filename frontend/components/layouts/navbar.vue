44a820d541105afb79546f109c1f02db61718b74
Victor_Hugo_W.C.M <vichwcm@gmail.com>
2024-12-31 14:03:02 -0300

    [add] navbar

--- frontend/components/layouts/navbar.vue
+++ frontend/components/layouts/navbar.vue
@@ -1,40 +1,65 @@
 <template>
   <v-container fluid class="navbar-wrapper">
     <v-container class="navbar-logo">
-      <!-- <v-img src="../../assets/a" alt="Logo" /> -->
+      <span class="navbar-title">Ações em Foco</span>
+      <v-icon color="#ffde59">mdi-magnify</v-icon>
     </v-container>
-    <v-container class="navbar-login"
-      ><v-btn class="navbar-button-login" variant="tonal"> Login </v-btn>
+    <v-container class="navbar-login">
+      <v-btn class="navbar-button-login" variant="tonal"> Login </v-btn>
       <v-btn class="navbar-button-sign_up" variant="tonal"> Sing up </v-btn>
     </v-container>
   </v-container>
 </template>
 <script setup></script>
 <style scoped>
+@font-face {
+  font-family: "leagueSpartan";
+  src: url("@/assets/fonts/League_Spartan/LeagueSpartan-VariableFont_wght.ttf")
+    format("truetype");
+}
+
 .navbar-wrapper {
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 1%;
   color: white;
-  background-color: black;
+  background-color: rgb(15, 15, 15);
   position: fixed;
   top: 0;
-  right: 0px;
-  width: calc(100vw - 1%);
-  height: 3%;
+  left: 0;
+  width: 100vw;
+  height: 7vh;
   border-bottom: 0.5vh solid #ffde59;
+}
+
+#navbar-logo {
+  height: 50px;
+  width: 50px;
+}
+
+.navbar-title {
+  font-family: "leagueSpartan", sans-serif;
+  font-size: 3vh;
 }
 
 .navbar-login {
   display: flex;
   gap: 0.5vw;
+  justify-content: end;
+  align-items: center;
 }
 
 .navbar-button-login {
-  background-color: rgb(103, 101, 94);
+  background-color: rgb(83, 80, 73) !important;
+  color: rgb(255, 255, 255);
+  height: 4vh !important;
+  width: auto !important;
 }
 .navbar-button-sign_up {
-  background-color: #ffde59;
+  background-color: #ffde59 !important;
+  color: rgb(48, 46, 46) !important;
+  height: 4vh !important;
+  width: auto !important;
 }
 </style>
